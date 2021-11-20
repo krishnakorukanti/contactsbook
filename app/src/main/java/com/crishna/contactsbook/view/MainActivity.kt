75818d42497210c01bc2175de5f86554da7bf3e6
Sai Krishna <crishnakorukanti@gmail.com>
2021-11-21 02:53:37 +0530

    ViewBinding Added

--- app/src/main/java/com/crishna/contactsbook/view/MainActivity.kt
+++ app/src/main/java/com/crishna/contactsbook/view/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.crishna.contactsbook.view
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.crishna.contactsbook.R
+import com.crishna.contactsbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
